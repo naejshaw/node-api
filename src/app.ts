707313a41580927d02530296a2339fd78e1d3f4d
Jean <89807593+naejshaw@users.noreply.github.com>
2024-09-22 18:20:19 -0300

    "Added cors middleware to app, updated package.json and package-lock.json"

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './Routes/Routes';
+import cors from 'cors';
 
 function createApp(){
 
@@ -8,6 +9,13 @@ function createApp(){
     app.use(express.json())
 
     app.use('/api', router)
+
+    const corsOptions = {
+        origin: '*',
+        methods: ['GET'],
+    }
+
+    app.use(cors(corsOptions))
 
     return app
 
