c8c69e2ffd948ef80515b84a80c5abe9afdf9733
Jean <89807593+naejshaw@users.noreply.github.com>
2024-10-08 15:05:43 +0000

    update controller

--- src/Controllers/ChordfyController.ts
+++ src/Controllers/ChordfyController.ts
@@ -3,21 +3,21 @@ import * as ChordListsService from '../Services/ChordListsService'
 import { SongsChordsModel } from '../Models/SongsChordsModel'
 
 export const getAllChordLists = async (req: Request, res: Response) =>{
-    const httpResponse = await ChordListsService.getAllChordListsService()
+    const httpResponse = await ChordListsService.getAllChordlistsService()
 
     res.status(httpResponse.statusCode).json(httpResponse.body)
 }
 
 export const getChordListById = async (req: Request, res: Response) =>{
     const id = parseInt(req.params.id)
-    const httpResponse = await ChordListsService.getChordListByIdService(id)
+    const httpResponse = await ChordListsService.getChordlistByIdService(id)
     res.status(httpResponse.statusCode).json(httpResponse.body)
 
 }
 
 export const createChordList = async (req: Request, res: Response) =>{
     const bodyValue = req.body
-    const httpResponse = await ChordListsService.createChordListService(bodyValue)
+    const httpResponse = await ChordListsService.createChordlistService(bodyValue)
     if(httpResponse){
         res.status(httpResponse.statusCode).json(httpResponse.body)
     }
@@ -26,12 +26,12 @@ export const createChordList = async (req: Request, res: Response) =>{
 export const updateChordListById = async (req: Request, res: Response) =>{
     const id = parseInt(req.params.id)
     const bodyValue: SongsChordsModel = req.body
-    const httpResponse = await ChordListsService.updateChordListByIdService(id, bodyValue)
+    const httpResponse = await ChordListsService.updateChordlistByIdService(id, bodyValue)
     res.status(httpResponse.statusCode).json(httpResponse.body)
 }
 
 export const deleteChordListById = async (req: Request, res: Response) =>{
     const id = parseInt(req.params.id)
-    const httpResponse = await ChordListsService.deleteChordListByIdService(id)
+    const httpResponse = await ChordListsService.deleteChordlistByIdService(id)
     res.status(httpResponse.statusCode).json(httpResponse.body)
 }
