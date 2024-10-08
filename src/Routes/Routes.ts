17b82109d56139475d51705b9ac5473b861c6817
Jean <89807593+naejshaw@users.noreply.github.com>
2024-10-08 14:21:34 +0000

    chords & routes update

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 import * as PlayersController from '../Controllers/PlayersController'
 import * as TeamsController from '../Controllers/TeamsController'
+import * as ChordsController from '../Controllers/ChordsController'
+import * as PlaylistsController from '../Controllers/PlaylistsController'
+import * as ChordfyController from '../Controllers/ChordfyController'
 
 const router = Router()
 
@@ -14,5 +17,16 @@ router.get('/teams', TeamsController.getListTeams)
 router.get('/teams/:id', TeamsController.getTeamById)
 router.post('/teams', TeamsController.createTeam)
 router.delete('/teams/:id', TeamsController.deleteTeamById)
+
+router.get('/cifraclub/:path', ChordsController.getChordById)
+
+router.get('/spotify/playlist', PlaylistsController.getAllPlaylists)
+router.get('/spotify/playlist/:id', PlaylistsController.getPlaylistById)
+
+router.get('/chordfy/playlists', ChordfyController.getAllChordlists)
+router.post('/chordfy/playlists', ChordfyController.createChordlist)
+router.get('/chordfy/playlists/:id', ChordfyController.getChordlistById)
+router.patch('/chordfy/playlists/:id', ChordfyController.updateChordlistById)
+router.delete('/chordfy/playlists/:id', ChordfyController.deleteChordlistById)
 
 export default router
